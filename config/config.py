f0a3dd64ddfa636fbeeed3ce2095b1dd72ab742e
Pavel Martynov <pm@steepshot.io>
2021-09-01 23:40:37 +0300

    fix errors and add webui last fixes

--- config/config.py
+++ config/config.py
@@ -1,12 +1,14 @@
+import os
+
 class NEBULA_CONF:
     def __init__(self) -> None:
-        self.ARANGO_HOST = 'http://ec2-18-158-123-0.eu-central-1.compute.amazonaws.com:8529'
-        self.ARANGO_DB = "nebula_development"
-        self.ELASIC_HOST = 'http://tnnb2_master:NeBuLa_2@ec2-18-158-123-0.eu-central-1.compute.amazonaws.com:9200/'
-        self.ELASTIC_INDEX = "datadriven"
-        self.S3BUCKET = "nebula-frames"
-        self.MLV_SERVER = '172.31.7.226'
-        self.MLV_PORT = '19530'  # default value
+        self.ARANGO_HOST = os.getenv('ARANGO_HOST', 'http://ec2-18-158-123-0.eu-central-1.compute.amazonaws.com:8529')
+        self.ARANGO_DB = os.getenv('ARANGO_DB', "nebula_development")
+        self.ELASIC_HOST = os.getenv('ELASIC_HOST', 'http://tnnb2_master:NeBuLa_2@ec2-18-158-123-0.eu-central-1.compute.amazonaws.com:9200/')
+        self.ELASTIC_INDEX = os.getenv('ELASTIC_INDEX', "datadriven")
+        self.S3BUCKET = os.getenv('S3BUCKET', "nebula-frames")
+        self.MLV_SERVER = os.getenv('MLV_SERVER', '172.31.7.226')
+        self.MLV_PORT = os.getenv('MLV_PORT', '19530')  # default value
 
     def get_database_name(self):
         return(self.ARANGO_DB)
