520976a448f6b17f72253e28b214aaf913f2e202
Dima Sivov <dsivov@gmail.com>
2022-01-13 08:47:41 +0000

    setup added to git

---
+++ nebula_api/mdmmt_api/setup.sh
@@ -0,0 +1,10 @@
+imkdir -p ckpts
+# https://github.com/facebookresearch/VMZ/
+wget https://github.com/bjuncek/VMZ/releases/download/test_models/irCSN_152_ig65m_from_scratch_f125286141.pth -O ckpts/irCSN_152_ig65m_from_scratch_f125286141.pth
+
+# https://github.com/tensorflow/models/tree/master/research/audioset/vggish
+wget https://storage.googleapis.com/audioset/vggish_model.ckpt -O ckpts/vggish_model.ckpt
+
+git clone https://github.com/openai/CLIP models/CLIP
+git clone https://github.com/tensorflow/models/ models/tensorflow_models
+
