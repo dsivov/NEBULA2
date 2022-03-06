7a5111b1684241dc29ce4e2b8a45f5f6a22e69a7
Gil <gilled@berkeley.edu>
2022-03-06 07:17:56 +0000

    new notebooks and minor interface change to vlm

--- nebula_api/vlmapi.py
+++ nebula_api/vlmapi.py
@@ -18,7 +18,8 @@ from vcomet.vcomet import VCOMET_KG
 class VLM_API:
     def __init__(self, model_name='mdmmt_mean', vcomet=None):
         self.available_class_names = ['clip_vit', 'clip_rn', 'mdmmt_max', 'mdmmt_mean', 'mdmmt_legacy']
-        self.vcomet = vcome if vcomet else VCOMET_KG()
+        self.vcomet = vcomet if vcomet else VCOMET_KG()
+        self.model_name = model_name
         if model_name not in self.available_class_names:
             raise Exception(f"Model name invalid. Use one of these names: {self.available_class_names}")
         if model_name == "clip_vit":
@@ -63,7 +64,9 @@ class VLM_API:
         return vggish_model, vmz_model, clip_model, model_vid, t_start, t_end, fps, encode_type
 
 
-    def encode_video(self, mid, scene_element, class_name='mdmmt_mean'):
+    def encode_video(self, mid, scene_element, class_name=None):
+        if not class_name:
+            class_name = self.model_name
         movie_info, fps, fn = self.download_and_get_minfo(mid, to_print=True)
         path = fn
         if class_name == 'mdmmt_max' or class_name == 'mdmmt_mean' or class_name == 'mdmmt_legacy':
@@ -85,7 +88,9 @@ class VLM_API:
             raise Exception("Class name you entered was not found.")
         return vid_embedding
     
-    def encode_text(self, text, class_name='mdmmt_mean'):
+    def encode_text(self, text, class_name=None):
+        if not class_name:
+            class_name = self.model_name
         if class_name == 'clip_rn':
             text_embedding = self.clip_rn.clip_batch_encode_text(text)
             text_embedding = torch.stack(text_embedding,axis=0)
