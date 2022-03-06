f6f30bea643138e32a9eb3721aab9cf7ab784ed1
michael kolomenkin <michael.kolomenkin@gmail.com>
2022-03-06 11:11:22 +0200

    LSMDC tests

--- pipeline/clip_cap.py
+++ pipeline/clip_cap.py
@@ -123,6 +123,7 @@ def generate2(
 
                 outputs = model.gpt(inputs_embeds=generated)
                 logits = outputs.logits
+                # logits = logits / (temperature if temperature > 0 else 1.0)
                 logits = logits[:, -1, :] / (temperature if temperature > 0 else 1.0)
                 sorted_logits, sorted_indices = torch.sort(logits, descending=True)
                 cumulative_probs = torch.cumsum(nnf.softmax(sorted_logits, dim=-1), dim=-1)
@@ -135,6 +136,7 @@ def generate2(
                 indices_to_remove = sorted_indices[sorted_indices_to_remove]
                 logits[:, indices_to_remove] = filter_value
                 next_token = torch.argmax(logits, -1).unsqueeze(0)
+                # pp = torch.argsort(logits, -1).unsqueeze(0)
                 next_token_embed = model.gpt.transformer.wte(next_token)
                 if tokens is None:
                     tokens = next_token
