7f5be99cc6093bb11911201f716028eb5faf1444
Dima Sivov <dsivov@gmail.com>
2022-03-07 15:20:55 +0000

    added actions

--- vcomet/vcomet_load.py
+++ vcomet/vcomet_load.py
@@ -42,59 +42,40 @@ class VCOMET_LOAD:
                 self.milvus_places.insert_vectors([vector.tolist()[0]], [meta])
                 #print(meta)
                 #input()
+
     def load_vit_vcomet_actions(self):    
         query = 'FOR doc IN vcomet_kg RETURN DISTINCT doc'
         print(query)
+        actions = []
         cursor = self.vc_db.aql.execute(query)    
-        for vcs in cursor:
+        for doc in cursor:
+            if 'intent' in doc:
+                for intent in doc['intent']:
+                    actions.append(intent)
+            if 'before' in doc:
+                for before in doc['before']:
+                    actions.append(before)
+            if 'after' in doc:
+                for after in doc['after']:
+                    actions.append(after)
+        actions = list(dict.fromkeys(actions))
+       
+        for vc in actions:
+            if len(vc.split()) < 9: 
+                print(vc) 
+                vector = self.vlmodel.encode_text(vc, class_name='clip_vit')
+                #print(len(vector.tolist()[0]))
+                meta = {
+                            'filename': 'none',
+                            'movie_id':'none',
+                            'nebula_movie_id': 'none',
+                            'stage': 'none',
+                            'frame_number': 'none',
+                            'sentence': vc,
+                        }
+                self.milvus_actions.insert_vectors([vector.tolist()[0]], [meta])
 
-            if 'intent' in vcs:
-                for vc in vcs['intent']:
-                    if len(vc.split()) < 9: 
-                        print(vc) 
-                        vector = self.vlmodel.encode_text(vc, class_name='clip_vit')
-                        print(len(vector.tolist()[0]))
-                        meta = {
-                                    'filename': 'none',
-                                    'movie_id':'none',
-                                    'nebula_movie_id': 'none',
-                                    'stage': 'none',
-                                    'frame_number': 'none',
-                                    'sentence': vc,
-                                }
-                        self.milvus_actions.insert_vectors([vector.tolist()[0]], [meta])
-
-            if 'after' in vcs:
-                for vc in vcs['intent']:
-                    if len(vc.split()) < 9: 
-                        print(vc) 
-                        vector = self.vlmodel.encode_text(vc, class_name='clip_vit')
-                        print(len(vector.tolist()[0]))
-                        meta = {
-                                    'filename': 'none',
-                                    'movie_id':'none',
-                                    'nebula_movie_id': 'none',
-                                    'stage': 'none',
-                                    'frame_number': 'none',
-                                    'sentence': vc,
-                                }
-                        self.milvus_actions.insert_vectors([vector.tolist()[0]], [meta])
-
-            if 'before' in vcs:
-                for vc in vcs['intent']:
-                    if len(vc.split()) < 9: 
-                        print(vc) 
-                        vector = self.vlmodel.encode_text(vc, class_name='clip_vit')
-                        print(len(vector.tolist()[0]))
-                        meta = {
-                                    'filename': 'none',
-                                    'movie_id':'none',
-                                    'nebula_movie_id': 'none',
-                                    'stage': 'none',
-                                    'frame_number': 'none',
-                                    'sentence': vc,
-                                }
-                        self.milvus_actions.insert_vectors([vector.tolist()[0]], [meta])
+           
 def main():
     kg = VCOMET_LOAD()
     #kg.load_vit_vcomet_place()
